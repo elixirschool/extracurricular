135c42991a08c571d3b6b55b9274effb99620776
Mike G <systems.support@gmail.com>
2017-08-20 21:24:21 -0700

    Splash page (#12)
    * adding sass-brunch
    
    * adding a splash of bourbon
    
    * change name to extracurricular
    
    * updated test

--- apps/web/assets/brunch-config.js
+++ apps/web/assets/brunch-config.js
@@ -47,6 +47,11 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
+    },
+    sass: {
+      options: {
+        includePaths: ['static/_scss']
+      }
     }
   },
 
