1cf7efa6d472c317671fa772a2f3f3534f58ea32
burden <burden@burden.cc>
2017-08-22 23:39:38 -0700

    improved opportunities_controller_test

--- apps/web/test/web/controllers/opportunities_controller_test.exs
+++ apps/web/test/web/controllers/opportunities_controller_test.exs
@@ -1,8 +1,22 @@
 defmodule Web.OpportunitiesControllerTest do
   use Web.ConnCase
 
+  import Data.Factory
+
+  alias Data.Opportunities
+
   test "GET /list", %{conn: conn} do
+    attributes = %{
+      level: "beginner",
+      project_id: insert(:project).id,
+      title: "Example Opportunity",
+      url: "https://example.com/tracker/1"
+    }
+    Opportunities.insert(attributes)
+    Opportunities.insert(%{attributes | title: "Another Opportunity"})
+
     conn = get conn, "/list"
-    assert html_response(conn, 200) =~ "Extracurricular"
+    assert html_response(conn, 200) =~ "Example Opportunity"
+    assert html_response(conn, 200) =~ "Another Opportunity"
   end
 end
