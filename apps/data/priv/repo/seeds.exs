4e31676538b031a6d8ef42ca079b8408f1cd0ae0
Bobby Grayson <bobbygrayson@gmail.com>
2017-09-03 15:35:45 -0500

    Write basic dev setup directions in README and set up database seeding
    Currently, we lack any documentation on how to get the project running
    in development. And, if one were to, they would be greeted with an empty
    page. This alleviates that issue by simple adding a basic database seed and
    documenting the setup process.

--- apps/data/priv/repo/seeds.exs
+++ apps/data/priv/repo/seeds.exs
@@ -9,3 +9,50 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+project_params_1 = %{
+  name: "Elixir School",
+  tags: ["elixir"],
+  url: "https://github.com/elixirschool/elixirschool",
+}
+
+project_params_2 = %{
+  name: "Extracurricular",
+  tags: ["elixir", "open source"],
+  url: "https://github.com/elixirschool/extracurricular",
+}
+
+project_params_3 = %{
+  name: "Appendix",
+  tags: ["elixir", "open source", "blog"],
+  url: "https://github.com/elixirschool/appendix",
+}
+
+{:ok, elixir_school}   = Data.Projects.insert(project_params_1)
+{:ok, extracirricular} = Data.Projects.insert(project_params_2)
+{:ok, appendix}        = Data.Projects.insert(project_params_3)
+
+opportunity_params_1 = %{
+  title: "Understanding GenServers and State",
+  level: 1,
+  url: "https://github.com/elixirschool/appendix/issues/1",
+  project_id: appendix.id
+}
+
+opportunity_params_2 = %{
+  title: "Translate to pig latin",
+  level: 1,
+  url: "https://github.com/elixirschool/elixirschool/issues/1",
+  project_id: elixir_school.id
+}
+
+opportunity_params_3 = %{
+  title: "Implement API for sorting",
+  level: 1,
+  url: "https://github.com/rails/rails/issues/1",
+  project_id: extracirricular.id
+}
+
+{:ok, _} = Data.Opportunities.insert(opportunity_params_1)
+{:ok, _} = Data.Opportunities.insert(opportunity_params_2)
+{:ok, _} = Data.Opportunities.insert(opportunity_params_3)
