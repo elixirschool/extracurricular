e0d4471d495f74c6c8a8c1725fcce9a0681502fe
Ivan Santos <pragmaticivan@gmail.com>
2017-09-09 13:59:28 -0700

    Adds and configure Dialyzer
    fixes #45

--- mix.exs
+++ mix.exs
@@ -5,12 +5,14 @@ defmodule Extracurricular.Mixfile do
     [apps_path: "apps",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     dialyzer: [plt_add_deps: :transitive],
      deps: deps()]
   end
 
   defp deps do
     [
-      {:credo, "~> 0.8.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 0.8.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
 end
