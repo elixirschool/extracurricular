56b8f98f5c4ffaf57a05f2f92f8aa13cfe9040b7
Dmitry <nscyclone@users.noreply.github.com>
2017-09-11 18:20:27 +0300

    Add AppSignal monitoring to Bots project (#48)
    * Add AppSignal monitoring to Bots project
    
    * Make Credo happy

--- apps/bot/lib/bot/clients/github.ex
+++ apps/bot/lib/bot/clients/github.ex
@@ -4,6 +4,7 @@ defmodule Bot.Client.GitHub do
   """
 
   use HTTPoison.Base
+  import Appsignal.Instrumentation.Helpers, only: [instrument: 4]
 
   def process_response_body(body), do: Poison.decode!(body)
 
@@ -12,6 +13,28 @@ defmodule Bot.Client.GitHub do
   end
 
   def process_url(url), do: api_url() <> url
+
+  def request(method, url, headers, body, options) do
+    transaction = Appsignal.Transaction.start(
+      Appsignal.Transaction.generate_id,
+      :background_task
+    )
+
+    transaction
+    |> Appsignal.Transaction.set_action("GitHub/request")
+    |> Appsignal.Transaction.set_sample_data(
+      "environment", %{request_path: url}
+    )
+
+    result = instrument(transaction, "github.request_api", "Requesting GitHub API", fn() ->
+      super(method, url, headers, body, options)
+    end)
+
+    Appsignal.Transaction.finish(transaction)
+    :ok = Appsignal.Transaction.complete(transaction)
+
+    result
+  end
 
   defp api_url, do: Application.get_env(:bot, :github_url)
 end
