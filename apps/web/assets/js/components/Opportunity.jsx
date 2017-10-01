45348172e48667ca520f030f139a05044a6cc9de
Chris Freeman <cfreeman.code@gmail.com>
2017-10-01 13:13:23 -0500

    Return `project.url` from APi and consume in UI

--- apps/web/assets/js/components/Opportunity.jsx
+++ apps/web/assets/js/components/Opportunity.jsx
@@ -23,7 +23,7 @@ export default class Opportunity extends React.Component {
           <header className="opportunity__header">
             <h2 className="opportunity__title">
               <span className="opportunity__project">
-                <a href={url}>{project.name}</a>
+                <a href={project.url}>{project.name}</a>
                 <span className="opportunity__title-icon">&#9655;</span>
               </span>
               <a href={url}>{title}</a>
